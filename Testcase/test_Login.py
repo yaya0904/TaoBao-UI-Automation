4ee787a184e9409ebccb7f6bff7a8723f5996f2e
jfang <1059081636@qq.com>
2020-12-15 16:26:01 +0800

    add selenium chromeoption

--- Testcase/test_Login.py
+++ Testcase/test_Login.py
@@ -11,6 +11,7 @@ import unittest
 import ddt
 import sys
 from utility import excel, HTMLTestRunner
+from selenium.webdriver import ChromeOptions
 
 path = sys.path[0]
 ex = excel.ExcelUtil(path + r'/Case/case_login.xls', 'different login')
@@ -19,7 +20,7 @@ ex = excel.ExcelUtil(path + r'/Case/case_login.xls', 'different login')
 class Login(unittest.TestCase):
     def setUp(self):
         self.resultpath = '/Users/yaya/Desktop/TaoBao UI automation/screenshot'
-        self.driver = webdriver.Chrome(r'/Users/yaya/Desktop/ChromeDriver/chromedriver')
+        self.driver = webdriver.Chrome()
         self.driver.get('https://www.taobao.com')
         self.loginOpe = loginUser.Login(self.driver)
         self.main = mainPage.Main(self.driver)
@@ -57,6 +58,7 @@ class Login(unittest.TestCase):
                 return
             else:
                 print('-------start to test %s--------'%casenum)
+                ChromeOptions.add_experimental_option('excludeSwitches'['enable-automation'])
                 if self.loginOpe.is_login():
                     self.loginOpe.logout_User()
                 self.loginOpe.login_User(username, password, loginresult)
